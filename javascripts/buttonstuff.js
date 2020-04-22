47a211b42957595141a03ab26278508bcd5f2f31
Hans H. Sievertsen <hhsievertsen@users.noreply.github.com>
2020-04-22 16:52:04 +0100

    added charts

--- javascripts/buttonstuff.js
+++ javascripts/buttonstuff.js
@@ -1,26 +1,25 @@
 var acc = document.getElementsByClassName("material1");
 var i;
 for (i = 0; i < acc.length; i++) {
-    acc[i].onclick = function(){
-           this.classList.toggle("active");
-           a=this.parentNode.nextElementSibling.classList;
-           a.toggle("show")
-      }
+    acc[i].onclick = function() {
+        this.classList.toggle("active");
+        this.parentNode.nextElementSibling.classList.toggle("show");
+    }
 }
 
 var acc = document.getElementsByClassName("material2");
 var i;
 for (i = 0; i < acc.length; i++) {
-    acc[i].onclick = function(){
-           this.classList.toggle("active");
-           this.parentNode.nextElementSibling.nextElementSibling.classList.toggle("show");
-      }
+    acc[i].onclick = function() {
+        this.classList.toggle("active");
+        this.parentNode.nextElementSibling.nextElementSibling.classList.toggle("show");
+    }
 }
 var acc = document.getElementsByClassName("material3");
 var i;
 for (i = 0; i < acc.length; i++) {
-    acc[i].onclick = function(){
-           this.classList.toggle("active");
-           this.parentNode.nextElementSibling.nextElementSibling.nextElementSibling.classList.toggle("show");
-      }
+    acc[i].onclick = function() {
+        this.classList.toggle("active");
+        this.parentNode.nextElementSibling.nextElementSibling.nextElementSibling.classList.toggle("show");
+    }
 }
