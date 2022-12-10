732eb083463206dcb0b7ddb20a005b27c5e9aa14
Hans H. Sievertsen <hhsievertsen@users.noreply.github.com>
2022-12-10 10:43:55 +0000

    new fig

---
+++ dataviz/20221204/fig.R
@@ -0,0 +1,25 @@
+# libraries
+library("tidyverse")
+library("ggthemes")
+library("viridis")
+library("stringr")
+library("gghighlight")
+# working directory
+# load data
+df<-read_csv("afe323d4-6ea0-470a-b9a8-3f7fecf75051_Data.csv")%>%
+   select(-66)%>%
+   pivot_longer(cols=5:65,names_to="Year",values_to="Value")%>%
+   mutate(year=as.numeric(substr(Year,1,4)))%>%
+  filter(!is.na(`Country Code`))
+# Chart
+
+ggplot()+
+      geom_line(df,mapping=aes(y=Value,x=year,group=`Country Name`))+
+      gghighlight(use_direct_label = F) +
+      facet_wrap(~ `Country Name`)+
+      theme_tufte()+
+      scale_x_continuous(breaks=seq(1960,2020,25))+
+     labs(y="Life expectancy at birth", x="",
+          caption="Data source: The World Bank")
+
+ggsave("fig.png")
